2b25e3526c435971c2e9aca46ae1b31781f6dc82
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-06-17 16:58:43 -0500

    Update worker.tf

--- worker.tf
+++ worker.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "example" {
-  name = "example"
+resource "aws_iam_role" "eks_worker_node" {
+  name = "eks_worker_node"
 
 assume_role_policy = <<POLICY
 {
@@ -19,40 +19,40 @@ POLICY
 }
 
 
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
+resource "aws_iam_role_policy_attachment" "eks_worker_node-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = "${aws_iam_role.example.name}"
+  role       = "${aws_iam_role.eks_worker_node.name}"
 }
 
-resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
+resource "aws_iam_role_policy_attachment" "eks_worker_node-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = "${aws_iam_role.example.name}"
+  role       = "${aws_iam_role.eks_worker_node.name}"
 }
 
-resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
+resource "aws_iam_role_policy_attachment" "eks_worker_node-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = "${aws_iam_role.example.name}"
+  role       = "${aws_iam_role.eks_worker_node.name}"
 }
 
-resource "aws_eks_node_group" "example" {
-  cluster_name    = "${aws_eks_cluster.example.name}"
-  node_group_name = "example"
-  node_role_arn   = "${aws_iam_role.example.arn}"
+resource "aws_eks_node_group" "eks_worker_node" {
+  cluster_name    = "${aws_eks_cluster.eks_master.name}"
+  node_group_name = "eks_worker_node"
+  node_role_arn   = "${aws_iam_role.eks_worker_node.arn}"
   subnet_ids      = "${var.subnet_ids}"
   instance_types  = ["${var.instance_type}"]
   
 
   scaling_config {
-    desired_size = "${var.asg_desired_capacity}"
-    max_size     = "${var.asg_max_size}"
-    min_size     = "${var.asg_min_size}"
+    desired_size = "${var.node_desired_capacity}"
+    max_size     = "${var.node_max_size}"
+    min_size     = "${var.node_min_size}"
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
-    "aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy",
-    "aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy",
-    "aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly",
+    "aws_iam_role_policy_attachment.eks_worker_node-AmazonEKSWorkerNodePolicy",
+    "aws_iam_role_policy_attachment.eks_worker_node-AmazonEKS_CNI_Policy",
+    "aws_iam_role_policy_attachment.eks_worker_node-AmazonEC2ContainerRegistryReadOnly",
   ]
 }
