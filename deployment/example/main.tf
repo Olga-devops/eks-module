c194f1b25feeefaa5f31be10bc095c3e90ac38ee
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-07-04 16:40:48 -0500

    Update main.tf

--- deployment/example/main.tf
+++ deployment/example/main.tf
@@ -1,7 +1,6 @@
 module "cluster-eks" {
   source = "../../"
-  vpc_id = "vpc-fb85df81"
-  subnet_ids = ["subnet-ec6b7fc2", "subnet-e2e6e885"]
+  subnet_ids = ["subnet-", "subnet-"]
   region = "us-east-1"
   ssh_key_location = "${file("~/.ssh/id_rsa.pub")}"
   cluster_name = "olga-eks-module-test"
@@ -10,6 +9,5 @@ module "cluster-eks" {
   node_desired_capacity = "2"
   node_min_size = "2"
   node_max_size = "5"
-  #s3_bucket = "eks-olga-project"
-  #security_group_ids = ["sg-0df6c36b0593ec3a5"]
+  security_group_ids = ["sg-"]
 }
